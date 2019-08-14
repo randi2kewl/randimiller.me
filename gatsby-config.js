8da508ec2916611ecc6d4d4a6ed3652892810bd5
Randi Miller <randi@geekguy.co>
2019-08-13 22:20:02 -0400

    Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,7 +124,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-73379983-2' },
+      options: { trackingId: 'UA-26912986-1' },
     },
     {
       resolve: 'gatsby-plugin-google-fonts',
