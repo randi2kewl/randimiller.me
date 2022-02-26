7f9871de53c95261d2abce5e9a5e84c03a5006a3
Randi Miller <randi@geekguy.co>
2022-02-26 17:28:42 -0500

    Contact form placeholders

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -20,36 +20,45 @@ let phone = "";
           <div class="px-4 py-5 bg-white sm:p-6">
             <div class="grid grid-cols-6 gap-6">
               <div class="col-span-6 sm:col-span-3">
-                <label for="first_name" class="block text-sm font-medium text-gray-700">First name</label>
+                  <label for="first_name" class="block text-sm font-medium text-gray-700">*First name</label>
                 <input 
                     type="text" 
                     name="first_name" 
                     id="first_name" 
                     autocomplete="given-name" 
                     class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                    bind:value={first_name}>
+                    placeholder="Jane"
+                    bind:value={first_name}
+                    required
+                >
               </div>
 
               <div class="col-span-6 sm:col-span-3">
-                <label for="last_name" class="block text-sm font-medium text-gray-700">Last name</label>
+                <label for="last_name" class="block text-sm font-medium text-gray-700">*Last name</label>
                 <input 
                    type="text" 
                    name="last_name" 
                    id="last_name" 
                    autocomplete="family-name" 
                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" 
-                bind:value={last_name}>
+                    placeholder="Doe"
+                   bind:value={last_name}
+                   required 
+                >
               </div>
 
               <div class="col-span-6 sm:col-span-4">
-                <label for="email" class="block text-sm font-medium text-gray-700">Email address</label>
+                <label for="email" class="block text-sm font-medium text-gray-700">*Email address</label>
                 <input 
                    type="email" 
                    name="email" 
                    id="email" 
                    autocomplete="email" 
                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                   bind:value={email}>
+                placeholder="email@example.com"
+                   bind:value={email}
+                   required 
+                >
               </div>
 
             <div class="col-span-6 sm:col-span-4">
@@ -59,8 +68,21 @@ let phone = "";
                 name="phone" 
                 id="phone" 
                 autocomplete="tel" 
+                placeholder="333-444-5555"
                 class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                 bind:value={phone}>
+            </div>
+
+
+            <div class="col-span-6 sm:col-span-4">
+              <label for="message" class="block text-sm font-medium text-gray-700">Message</label>
+              <textarea 
+                name="message" 
+                id="message"
+                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm
+                        border-gray-400 rounded-md"
+                placeholder="A short message about the project needs or information that I should know prior to contacting you back."
+                ></textarea>
             </div>
 
             </div>
