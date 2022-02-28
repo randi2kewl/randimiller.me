67c9999fef3c7f5c5474235e48ad7c6b85fbcb74
Randi Miller <randi@geekguy.co>
2022-02-28 13:10:11 -0500

    Form

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -13,7 +13,6 @@ let phone = "";
 <Title title="Contact Me" />
 
 <form class="contact-form" name="contact" method="POST" action="/contact-success" netlify-honeypot="bot-field" data-netlify="true">
-    <input type="hidden" name="contact" value="test" />
   <p class="hidden">
     <label>
       Don’t fill this out if you’re human: <input name="bot-field" />
